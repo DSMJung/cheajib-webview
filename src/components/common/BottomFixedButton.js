1618611fe2630e15f4dae8c823a33c7c13b5ab45
kimuichan <leerabi577@gmail.com>
2022-08-04 16:42:13 +0900

    bottom fixed button 추가

---
+++ src/components/common/BottomFixedButton.js
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+import DefaultButton from "./DefaultButton";
+
+const BottomFixedButton = ({
+  disable = false,
+  onClick,
+  children,
+  isFill = false,
+}) => {
+  return (
+    <>
+      <FullSizeButtonWrapper>
+        <DefaultButton disable={disable} onClick={onClick} isFill={isFill}>
+          {children}
+        </DefaultButton>
+      </FullSizeButtonWrapper>
+      <BackgroundBox />
+    </>
+  );
+};
+
+const FullSizeButtonWrapper = styled.section`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 0 20px 16px;
+`;
+
+const BackgroundBox = styled.div`
+  width: 100%;
+  height: 48px;
+`;
+
+export default BottomFixedButton;
