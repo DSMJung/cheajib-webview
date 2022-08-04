1b1e4e8ee57bd26dfbb35eb568d9d27bc64d7682
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-08-04 19:30:30 +0900

    mypage navigationBox 추가

---
+++ src/components/mypage/NavigationBox.js
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import Arrow from "../../assets/mypage/arrow.svg";
+
+const NavigationBox = ({ subText, mainText }) => {
+  return (
+    <NavigationWrapper>
+      <SubText>{subText}</SubText>
+      {mainText()}
+    </NavigationWrapper>
+  );
+};
+
+const NavigationWrapper = styled.div`
+  width: 100%;
+  height: 64px;
+  padding: 12px 20px;
+  background-image: url(${Arrow});
+  background-repeat: no-repeat;
+  background-position: 375px 50%;
+`;
+
+const SubText = styled.p`
+  font-size: 12px;
+  color: ${({ theme }) => theme.grey700};
+  margin-bottom: 8px;
+`;
+
+export default NavigationBox;
