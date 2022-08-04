e2f51d1f3e0266e72bbccfc86c0e8ab50559fe3c
kimuichan <leerabi577@gmail.com>
2022-08-04 16:42:01 +0900

    default container 추가

---
+++ src/components/common/DefaultContainer.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const DefaultContainer = styled.div`
+  position: absolute !important;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
