399f77c9c2bce4153ef7f63a9b30d5e202b5ec67
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-08-09 03:10:25 +0900

    restaurant detail page review 추가

---
+++ src/components/restaurantDetail/review/Review.js
@@ -0,0 +1,62 @@
+import styled from "@emotion/styled";
+import ReviewAnswer from "./ReviewAnswer";
+
+const Review = () => {
+  return (
+    <>
+      <ReviewWrapper>
+        <MeatballsMenu />
+        <UserName>김의찬</UserName>
+        <div style={{ display: "flex", margin: "8px 0 12px" }}>
+          <StarBox />
+          <Date>어제</Date>
+        </div>
+        <Content>
+          완전 맛있어요 최강이다 사장님 만수무강하세요~!! 완전 맛있어요 최강이다
+          사장님 만수무강하세요~!! 완전 맛있어요 최강이다 사장님
+          만수무강하세요~!!
+        </Content>
+      </ReviewWrapper>
+      <ReviewAnswer />
+    </>
+  );
+};
+
+const ReviewWrapper = styled.div`
+  position: relative;
+  margin: 0 18px 12px 20px;
+`;
+
+const UserName = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const StarBox = styled.div`
+  background-color: ${({ theme }) => theme.grey700};
+  height: 12px;
+  width: 68px;
+`;
+
+const Date = styled.p`
+  font-size: 12px;
+  margin-left: 8px;
+  color: ${({ theme }) => theme.grey700};
+`;
+
+const Content = styled.p`
+  font-size: 14px;
+  line-height: 18px;
+  color: ${({ theme }) => theme.grey900};
+`;
+
+const MeatballsMenu = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: ${({ theme }) => theme.grey700};
+  height: 24px;
+  width: 24px;
+`;
+
+export default Review;
