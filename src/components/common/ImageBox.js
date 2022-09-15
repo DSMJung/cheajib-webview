3f331b9338159f080038f43b99b4cf31b82754df
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-09-15 20:19:36 +0900

    이미지 불러오는 박스 추가

---
+++ src/components/common/ImageBox.js
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import Image from "../../assets/common/image.png";
+
+const ImageBox = ({ height, imageState, setImageState }) => {
+  const imageAdd = (fileBlob) => {
+    const url = URL.createObjectURL(fileBlob);
+    setImageState(url);
+  };
+  return (
+    <>
+      <ImageWrapper height={height} htmlFor="imageInput">
+        {imageState ? (
+          <img
+            src={imageState}
+            width="100%"
+            height="100%"
+            style={{ objectFit: "cover", objectPosition: "center" }}
+          />
+        ) : (
+          <img src={Image} style={{ width: "60px" }} />
+        )}
+      </ImageWrapper>
+      <input
+        type={"file"}
+        id="imageInput"
+        accept="image/*"
+        style={{ display: "none" }}
+        onChange={(e) => {
+          imageAdd(e.target.files[0]);
+        }}
+      ></input>
+    </>
+  );
+};
+
+const ImageWrapper = styled.label`
+  width: 100%;
+  overflow: hidden;
+  height: ${({ height }) => height};
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.grey500};
+`;
+
+export default ImageBox;
