b84d0caf3eb6afa758b7bd48907853e80be19206
kimuichan <leerabi577@gmail.com>
2022-09-19 19:40:28 +0900

    api fetcher 함수 추가

--- src/utils/api/generator.js
+++ src/utils/api/generator.js
@@ -1,9 +1,13 @@
+import axios from "axios";
 import queryKey from "../queryKey";
 import { instance } from "./instance";
 
 export const loginGenerator = async () => {
-  const generatorKey = queryKey.users.index();
-  return (await instance.post(generatorKey)).data;
+  return (
+    await axios.post(
+      "https://nid.naver.com/oauth2.0/authorize?client_id=wl9JLG_UF_57OZZgjkms&response_type=code&redirect_uri=http://3.34.174.96:8080/naver/redirect&state=STATE_STRING"
+    )
+  ).data;
 };
 
 //api 수정 필요
@@ -22,5 +26,32 @@ export const menuGenerator = async ({
     description,
     menu_image,
     level,
+  });
+};
+
+export const imageGenerator = async (BlobImages) => {
+  const generatorKey = queryKey.images.index();
+  const FNData = new FormData();
+  BlobImages.map((imageBlob) => FNData.append("images", imageBlob), {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return (await instance.post(generatorKey, FNData)).data;
+};
+
+export const reviewGenerator = ({
+  restaurent_id,
+  review_point,
+  image_list,
+  content,
+  menu_list,
+}) => {
+  const generatorKey = queryKey.review.restaurnat_id(restaurent_id);
+  return instance.post(generatorKey, {
+    review_point,
+    image_list,
+    content,
+    menu_list,
   });
 };
